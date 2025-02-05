1dd44058b50634b067a6253b8a550eb290297ce3
renathossain <renathossain@gmail.com>
2025-02-05 00:01:40 -0500

    Removed Audio XFCE settings

--- postinstall.sh
+++ postinstall.sh
@@ -163,8 +163,6 @@ sudo systemctl enable docker
 # XFCE Settings
 xfconf-query -c xsettings -p /Net/ThemeName -s Adwaita-dark
 xfconf-query -c xfce4-notifyd -p /compat/use-override-redirect-windows -t bool -s true --create
-xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/AudioLowerVolume -t string -s "wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%-" --create 
-xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/AudioRaiseVolume -t string -s "wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%+" --create
 
 ###### CLEANUP ######
 
