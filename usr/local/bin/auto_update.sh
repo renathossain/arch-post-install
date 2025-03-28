8bc3c03bb93e74d4ae9cda9d9ef18ae3eaafff0c
renathossain <renathossain@gmail.com>
2025-03-28 13:25:25 -0400

    Updated auto updater to remove older packages.

--- usr/local/bin/auto_update.sh
+++ usr/local/bin/auto_update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /usr/bin/yay -Syu --noconfirm
-/usr/bin/yay -Scc --noconfirm
 /usr/bin/yay --noconfirm -Rns $(/usr/bin/yay -Qdtq)
 /usr/bin/rm -rf /var/lib/systemd/coredump/*
+/usr/bin/yay -Scc --noconfirm
+/usr/bin/paccache -r -k 1
