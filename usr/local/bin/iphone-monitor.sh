b2af806bfdac79c0f117a0cfad37b38fdabe622a
renathossain <renathossain@gmail.com>
2025-06-29 12:34:42 -0400

    Updated iPhone Mounting Script.

--- usr/local/bin/iphone-monitor.sh
+++ usr/local/bin/iphone-monitor.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+# Mounts iPhone to directory
 IPHONE_STATUS="/var/lib/iphone/connection.status"
 MOUNT_POINT="$HOME/mnt/iPhone"
 
-mkdir -p "$MOUNT_POINT"
-
 while true; do
     inotifywait -e close_write "$IPHONE_STATUS" >/dev/null 2>&1
-
     if [[ "$(cat "$IPHONE_STATUS")" == "1" ]]; then
-        /usr/bin/sleep 2 && /usr/bin/ifuse "$MOUNT_POINT" # Access iPhone camera photos
+        mkdir -p "$MOUNT_POINT"
+        /usr/bin/sleep 2
+        /usr/bin/ifuse "$MOUNT_POINT" # Access Camera Photos and Videos
     elif [[ "$(cat "$IPHONE_STATUS")" == "0" ]]; then
         /usr/bin/fusermount -u "$MOUNT_POINT"
+        rmdir "$MOUNT_POINT"
     fi
 done
