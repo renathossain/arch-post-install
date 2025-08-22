fb54b80d95bdb7d66520f67e94bc0a2dd4f37e7b
renathossain <renathossain@gmail.com>
2025-08-22 15:32:48 -0400

    Inhibit shutdown during update.

--- usr/local/bin/auto_update.sh
+++ usr/local/bin/auto_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /usr/bin/yay -Syu --noconfirm
-/usr/bin/yay --noconfirm -Rns $(/usr/bin/yay -Qdtq)
+/usr/bin/yay --noconfirm -Rns $(/usr/bin/pacman -Qdtq)
 /usr/bin/rm -rf /var/lib/systemd/coredump/*
 /usr/bin/yay -Scc --noconfirm
 /usr/bin/paccache -r -k 1
